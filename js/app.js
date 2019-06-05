565b3dd1a83261ef48a623d41adfb17de9abd9b0
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-05 13:51:05 -0700

    my second commit

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
 'use strict'
 
-var myFirstName = prompt('what is my nick name, Roma or Rom?');
-console.log(myFirstName.toLowerCase());
-if(myFirstName === 'Rom' || myFirstName === 'ROM' || myFirstName === 'rom') {
+var myFirstName = prompt('Is my nick name is Roma?').toLowerCase();
+console.log(myFirstName);
+if(myFirstName === 'yes' || myFirstName === 'YES' || myFirstName === 'y') {
     alert('that\'s correct');
 } else {
-   alert('nope my nick name is Rom');
+   alert('WRONG! it is Roma.');
 }
 
-var myFavoriteColor = prompt('what is my favorite color, gray or black?');
-console.log(myFavoriteColor.toLowerCase());
-if(myFavoriteColor.toLowerCase() === 'gray') {
-     alert('yes! that is right, '+ myFavoriteColor + ' is my favorite color!')
+var myFavoriteColor = prompt('Is my favorite color is gray?').toLowerCase();
+console.log(myFavoriteColor);
+if(myFavoriteColor === 'yes' || myFavoriteColor === 'YES' || myFavoriteColor=== 'y') {
+     alert('yes! that is right!')
 } else { 
-    alert('NOPE! i hope one day you will figure it out! ')
+    alert('Sorry, it is!')
 }
 
-var myFavoriteAnimal = prompt('What is my favorite animal, dog or cat?');
-console.log(myFavoriteAnimal.toLowerCase());
-if(myFavoriteAnimal.toLowerCase() === 'dog') {
-    alert('yes! that\'s right, i love '+ myFavoriteAnimal + ' ! ')
+var myFavoriteAnimal = prompt('Do you think my favorite animal is Dog?').toLowerCase();
+console.log(myFavoriteAnimal);
+if(myFavoriteAnimal === 'yes' || myFavoriteAnimal === 'YES' || myFavoriteAnimal === 'y') {
+    alert('yes! that\'s right, i love dog and i have one! ')
 } else { 
-    alert('NOPE! i hope one day you will figure it out! ')
+    alert('NOPE! you should know this.')
 }
 
-var myFavoriteMovie = prompt('What is my favorite movie, Titanic or Lord of the ring?');
-console.log(myFavoriteMovie.toLowerCase());
-if(myFavoriteMovie.toLowerCase() === 'lord of the ring') {
-   alert('that\'s right, i have watched '+ myFavoriteMovie + ' many times. ')
+var myFavoriteMovie = prompt('Do you think i like to watch movies?').toLowerCase();
+console.log(myFavoriteMovie);
+if(myFavoriteMovie === 'yes' || myFavoriteMovie === 'YES' || myFavoriteMovie === 'y') {
+   alert('that\'s right, one of my favorite movie is Lord of the ring.')
 } else { 
-    alert('Wrong! ' + myFavoriteMovie + ' is my second favorite.')
+    alert('Wrong! It is my favorite.')
 }
 
-var myFavoriteSubject = prompt('What is my favorite subject, Math or Art?');
-console.log(myFavoriteSubject.toLowerCase());
-if(myFavoriteSubject.toLowerCase() === 'math') {
-   alert('that\'s right, i love '+ myFavoriteSubject + ' but it has been long since i took it')
+var myFavoriteSubject = prompt('Do you think i like Math\'s?').toLowerCase();
+console.log(myFavoriteSubject);
+if(myFavoriteSubject === 'yes' || myFavoriteSubject === 'YES' || myFavoriteSubject === 'y') {
+   alert('that\'s right, i like Math\'s but it has been long time since i took it.')
 } else { 
-   alert('Nope! but that is my second favorite! ')
+   alert('Nope! I hope you know now.')
 }
 
 
