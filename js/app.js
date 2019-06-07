1ee3ffe5ab58878b1a76465e19893615908b8255
Roman Gebrehiwot <rttgg1@gmail.com>
2019-06-07 16:59:38 -0700

    done with step 7

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 'use strict';
+var tries = 0;
+var yourName = prompt('Selam! How\'re you today? What\'s your name?');
+
 
 step1();
 step2();
@@ -9,15 +12,13 @@ step6();
 step7();
 
 
-
-var tries = 0;
-
 function step1(){
   var myFirstName = prompt('Is my nick name is Roma?').toLowerCase();
   console.log(myFirstName);
   if(myFirstName === 'yes' || myFirstName === 'YES' || myFirstName === 'y') {
     alert('that\'s correct');
     tries++;
+    console.log(tries);
   } else {
     alert('WRONG! it is Roma.');
   }
@@ -29,6 +30,7 @@ function step2(){
   if(myFavoriteColor === 'yes' || myFavoriteColor === 'YES' || myFavoriteColor=== 'y') {
     alert('yes! that is right!');
     tries++;
+    console.log(tries);
   } else {
     alert('Sorry, it is!');
   }
@@ -40,6 +42,7 @@ function step3(){
   if(myFavoriteAnimal === 'yes' || myFavoriteAnimal === 'YES' || myFavoriteAnimal === 'y') {
     alert('yes! that\'s right, i love dog and i have one! ');
     tries++;
+    console.log(tries);
   } else {
     alert('NOPE! you should know this.');
   }
@@ -51,6 +54,7 @@ function step4(){
   if(myFavoriteMovie === 'no' || myFavoriteMovie === 'n') {
     alert('that\'s right, one of my favorite movie is Lord of the ring.');
     tries++;
+    console.log(tries);
   } else {
     alert('Wrong! It is my favorite.');
   }
@@ -62,40 +66,22 @@ function step5(){
   if(myFavoriteSubject === 'yes' || myFavoriteSubject === 'YES' || myFavoriteSubject === 'y') {
     alert('that\'s right, i like Math\'s but it has been long time since i took it.');
     tries++;
+    console.log(tries);
   } else {
     alert('Nope! I hope you know now.');
   }
 }
-// function step6(){
 
-//   var numberOfTries = 0;
-//   while(numberOfTries < 4){
-//     var howManyMovies= prompt('Can you guess how many movies i have watched this year?');
-//     console.log(howManyMovies);
-//     if(howManyMovies === '5' ) {
-//       alert('that\'s right, i have watched ' + howManyMovies + ' movies!');
-//       break;
-//       //tries++;
-      
-//     } else if (howManyMovies > 5 ) {
-//       alert('that\'s too high!');
-//     } else if (howManyMovies < 5 ) {
-//       alert('that\'s too low');
-//     }
-//     //numberOfTries++;
-//   }
 
 function step6(){
   for ( var i = 0; i < 4; i++){
     var howManyMovies= prompt('Can you guess how many movies i have watched this year?');
     /*if # of tries are < 4 and if they didn't pick the right answer
-    and if they got the right answer alert "you got it" you got the right answer 
+    and if they got the right answer alert "you got it" you got the right answer
     break */
     if (howManyMovies === '5'){
       alert('you got it right!');
-      console.log('before break');
       break;
-      
     }
     else if (howManyMovies < 5){
       alert('that\'s too low!');
@@ -135,10 +121,10 @@ function step7(){
 
 }
 
-console.log(tries);
-if (tries < 6){
-  alert(name + ' WOW! You\'re very smart');
-  tries ++;
+if (tries === 6){
+  alert(yourName + ' WOW! You\'re very smart');
+  tries++;
+  console.log(tries);
 } else {
-  alert(name + ' you got ' + tries + ' out of 6 correct.');
+  alert(yourName + ' you got ' + tries + ' out of 6 correct.');
 }
